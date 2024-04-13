f413d16405f48614d826d4c9b882de1f28831749
KyungsooLee <jmtkd9196@gmail.com>
2024-04-13 21:40:15 +0900

    Nano Challenge 1 init

---
+++ FootballManager/FootballManager/FootballManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballManagerApp.swift
+//  FootballManager
+//
+//  Created by kyungsoolee on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FootballManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
